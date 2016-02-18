e3c690c45412b739110a1762b83940882ea1672e
pg.twitter <pg.twitter@gmail.com>
2016-02-18 20:25:31 +0900

    Dynamically to get the 'war' file name. To enable specify the port number in the properties.

--- src/main/java/my/Main.java
+++ src/main/java/my/Main.java
@@ -2,9 +2,15 @@ package my;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
@@ -19,9 +25,13 @@ import org.eclipse.jetty.webapp.WebInfConfiguration;
 import org.eclipse.jetty.webapp.WebXmlConfiguration;
 
 public class Main {
+	private static final int DEFALUT_PORT = 1234;
+
 	public static void main(String[] args) throws Exception {
-		String service = "HelloJersey";
-		int port = 1234;
+		int port = getPort();
+		String service = getWarFileName();
+		if (service == null)
+			return;
 
 		Server server = new Server(port);
 		try {
@@ -32,6 +42,32 @@ public class Main {
 		} finally {
 			server.destroy();
 		}
+	}
+
+	private static int getPort() {
+		int ret = DEFALUT_PORT;
+		String portString = System.getProperty("port");
+		if (portString != null) {
+			try {
+				ret = Integer.parseInt(portString);
+			} catch (NumberFormatException e) {
+				ret = DEFALUT_PORT;
+			}
+		}
+		return ret;
+	}
+
+	private static String getWarFileName() throws IOException {
+		ArrayList<String> ret = new ArrayList<String>();
+		ProtectionDomain domain = Main.class.getProtectionDomain();
+		URL jarLocation = domain.getCodeSource().getLocation();
+		for (Enumeration<JarEntry> e = new JarFile(jarLocation.getPath()).entries(); e.hasMoreElements();) {
+			String entryName = e.nextElement().getName();
+			if (entryName.endsWith(".war")) {
+				ret.add(entryName.substring(0, entryName.length() - 4));
+			}
+		}
+		return ret.get(0);
 	}
 
 	private static WebAppContext getWebAppContext(String service) throws IOException {
